cc92dcab0cb631b2091b7bf248f2030039dc9c0e
Madhav Paudel <poudelmadhav143@gmail.com>
2017-09-09 15:34:10 +0545

    database changed to gulariya-dcs and home added to navbar

--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,8 @@ ActiveRecord::Schema.define(version: 20170908174411) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
     t.text     "body"
-    t.string   "resource_id",   null: false
-    t.string   "resource_type", null: false
+    t.string   "resource_type"
+    t.integer  "resource_id"
     t.string   "author_type"
     t.integer  "author_id"
     t.datetime "created_at",    null: false
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 20170908174411) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
   end
@@ -57,14 +59,6 @@ ActiveRecord::Schema.define(version: 20170908174411) do
     t.string   "remarks"
   end
 
-  create_table "ideas", force: :cascade do |t|
-    t.string   "author"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "priority"
-  end
-
   create_table "records", force: :cascade do |t|
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
@@ -76,24 +70,6 @@ ActiveRecord::Schema.define(version: 20170908174411) do
     t.integer  "sn"
     t.integer  "get_pass"
     t.string   "remarks"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.string   "name"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
