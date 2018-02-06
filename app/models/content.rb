5c9e99a01fed3b109be806750e9ed52014859870
Madhav Paudel <poudelmadhav143@gmail.com>
2018-02-06 15:17:23 +0545

    uniqueness in sn added

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,6 +1,6 @@
 class Content < ApplicationRecord
 	validates :tr_sn, :sn, :received_date, :kva, :install_place, presence: true
-
+	validates_uniqueness_of :sn
 	self.per_page = 50
 	
 	def self.narayan
